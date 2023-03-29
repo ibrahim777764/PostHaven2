14239241bd76fa1796f459fab6c2fad658b115c1
ibrahim777764 <ibrahimnaseer896@gmail.com>
2023-03-29 20:47:11 +0500

    updated posthaven

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,4 +124,11 @@ CACHES = {
         'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
     }
 }
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'myapp', 'static'),
+]
+STATIC_ROOT = '/home/ibrahim/Documents/updated/PostHaven2/myproject/staticfiles/'
+
 
