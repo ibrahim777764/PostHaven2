86907f5127f27040a68c4c3a9561a0ab73a42d54
ibrahim777764 <ibrahimnaseer896@gmail.com>
2023-03-29 23:08:17 +0500

    styling webiste

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -3,8 +3,7 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html', redirect_authenticated_user=True), name='login'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
